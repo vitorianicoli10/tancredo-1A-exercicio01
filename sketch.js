029f3fc67976fe3b060b7f79e97293986b27bfb1
vitorianicoli10 <141413896+vitorianicoli10@users.noreply.github.com>
2023-08-14 12:04:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+  background("purple")
+}
+
+function draw() {
+  ;
+  
+ stroke("black");
+  fill("silver");
+  
+ if(mouseIsPressed){
+ rect(mouseX,mouseY,30,30);  
+ }
+ 
+  
+  
+}
